53588caf105986941699fba5cfd516c3fb846244
de.churkin <de.churkin@bars-open.ru>
2024-08-13 23:41:59 +0300

    feat: react-router-dom v 0.0.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+
 const App = () => {
     return (
         <div className='app'>
-            dasdsadsa
-            <Counter/>
+            <Link to={'/'} >Main</Link>
+            <Link to={'/about'} >About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
