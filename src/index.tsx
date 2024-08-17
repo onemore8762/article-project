22345a3ccae820c587136311f853db674dbb9d02
Denis Churkin <denis.churkin.afikei@mail.ru>
2024-08-18 00:01:49 +0300

    Test commit v 0.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import {createRoot} from "react-dom/client";
 import {App} from "./app/App";
 import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "./app/providers/ui/ThemeProvider";
+import {ThemeProvider} from "@/app/providers";
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
-root.render(<BrowserRouter>
-   <ThemeProvider>
-       <App/>
-   </ThemeProvider>
-</BrowserRouter>,);
+root.render(
+    <BrowserRouter>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
+    </BrowserRouter>
+);
